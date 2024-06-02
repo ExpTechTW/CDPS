56f6e98bc8eabd6e2bd1000b8ec5b94a94eaca92
whes1015 <whes1015@gmail.com>
2024-06-02 16:39:57 +0800

    feat: dependencies

--- cdps/plugin/manager.py
+++ cdps/plugin/manager.py
@@ -6,6 +6,7 @@ import sys
 import zipfile
 
 from cdps.utils.logger import Log
+from cdps.utils.version import Version
 
 directory_path = "./plugins/"
 
@@ -74,8 +75,20 @@ class Plugin():
                 data = json.load(file)
                 plugins_info[plugin] = data
 
-    def dependencies():
-        pass
+    def dependencies(self, plugins_info: list, plugins_list: list):
+        for plugin in plugins_list:
+            for key, value in plugins_info[plugin]['dependencies'].items():
+                if plugins_info[key] is None:
+                    self.log.logger.error(
+                        "Plugin {} Need Install Dependencies {} {}".format(plugin, key, value))
+                else:
+                    ver_use = Version(plugins_info[key]['version'])
+                    ver_need = Version(value.replace(">=", ""))
+
+                    if ver_use < ver_need:
+                        self.log.logger.error(
+                            "Plugin {} Need Upgrade Dependencies {} {}".format(plugin, key, value))
+                        plugins_list.remove(plugin)
 
     def load_plugins(self, plugins_list):
         loaded_plugins_list = []
