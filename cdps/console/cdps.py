e329588bca0dd67835e3ef31a6dad7b6a9fa41cd
whes1015 <whes1015@gmail.com>
2024-06-03 04:22:38 +0800

    feat: ipc

--- cdps/console/cdps.py
+++ cdps/console/cdps.py
@@ -1,14 +1,39 @@
 from cdps.plugin.manager import Plugin
+from cdps.utils.logger import Log
+from cdps.constants import core_constant
 
 
 class console_cdps():
     def __init__(self, args):
+        self.loop = True
         self.args = args
-        if self.args[1] == "plugin":
-            self.__plugin__()
+        self.log = Log()
+        if self.args[1]:
+            if self.args[1] == "plugin":
+                self.__plugin__()
+            elif self.args[1] == "version":
+                self.log.logger.info('{} {}'.format(
+                    core_constant.NAME, core_constant.VERSION))
+            elif self.args[1] == "exit":
+                self.loop = False
+                self.log.logger.warn('{} {} Good Bye!!!'.format(
+                    core_constant.NAME, core_constant.VERSION))
+            else:
+                self.log.logger.error("Unknow Command! ( cdps )")
+        else:
+            self.log.logger.error("Unknow Command! ( cdps )")
 
     def __plugin__(self):
-        if self.args[2] == "reload":
-            Plugin().reload_load_plugins(self.args[3])
+        if self.args[2]:
+            if self.args[2] == "reload":
+                Plugin().reload_load_plugins(self.args[3])
+            elif self.args[2] == "load":
+                self.log.logger.warning("Not Implemented! ( cdps plugin )")
+            elif self.args[2] == "unload":
+                self.log.logger.warning("Not Implemented! ( cdps plugin )")
+            else:
+                self.log.logger.error("Unknow Command! ( cdps plugin )")
+        else:
+            self.log.logger.error("Unknow Command! ( cdps plugin )")
 
 # cdps plugin reload example
---
+++ docs/zh/developer/update.md
@@ -0,0 +1,10 @@
+## 1.0.15
+### 🌟 新增
+- `onCommandEvent`
+- `cdps version` ( In Program Command )
+- `cdps exit` ( In Program Command )
+- `cdps plugin reload {plugin}` ( In Program Command )
+### 🔌 優化
+- Command 錯誤處理方式
+### 🐞 修正
+- Log 錯字
